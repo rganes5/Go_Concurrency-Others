d2cb2c117c4c5c3261ac10b32dcae96ee50f7d7e
Ganesh <ganeshraveendranit@gmail.com>
2023-09-09 18:30:49 +0530

    learning

---
+++ Interview1-slice/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func modifyArray(array [5]int) {
+	array[2] = 50
+	array[3] = 100
+}
+
+func modifySlice(slice []int) {
+	slice[2] = 50
+	slice[3] = 100
+}
+
+func main() {
+	array := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("Before modification", array)
+	modifyArray(array)
+	fmt.Println("After modification", array)
+
+	slice := []int{1, 2, 3, 4, 5}
+	fmt.Println("Before modification", slice)
+	modifySlice(slice)
+	fmt.Println("After modification", slice)
+
+}
