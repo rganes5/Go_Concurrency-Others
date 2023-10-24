acc15ae8da8fe6bf49737058bbcdf96d980a9b2a
Ganesh <ganeshraveendranit@gmail.com>
2023-10-24 14:05:34 +0530

    async task fixed with channel

---
+++ ConcurrencyAndGoroutines/stevehooks/async-task-fixed/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now()
+	ch := make(chan struct{})
+	go task1(ch)
+	go task2(ch)
+	go task3(ch)
+	go task4(ch)
+	<-ch
+	<-ch
+	<-ch
+	<-ch
+	//time.Sleep(time.Second)
+	fmt.Println("Elapsed", time.Since(now))
+
+}
+
+func task1(ch chan struct{}) {
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("task1")
+	ch <- struct{}{}
+}
+func task2(ch chan struct{}) {
+	time.Sleep(200 * time.Millisecond)
+	fmt.Println("task2")
+	ch <- struct{}{}
+
+}
+func task3(ch chan struct{}) {
+	fmt.Println("task3")
+	ch <- struct{}{}
+}
+func task4(ch chan struct{}) {
+	time.Sleep(100 * time.Millisecond)
+	fmt.Println("task4")
+	ch <- struct{}{}
+}
--- ConcurrencyAndGoroutines/stevehooks/async-task/main.go
+++ ConcurrencyAndGoroutines/stevehooks/async-task/main.go
@@ -13,21 +13,24 @@ func main() {
 	go task4()
 	time.Sleep(time.Second)
 	fmt.Println("Elapsed", time.Since(now))
-
 }
 
 func task1() {
 	time.Sleep(100 * time.Millisecond)
 	fmt.Println("task1")
+
 }
 func task2() {
 	time.Sleep(200 * time.Millisecond)
 	fmt.Println("task2")
+
 }
 func task3() {
 	fmt.Println("task3")
+
 }
 func task4() {
 	time.Sleep(100 * time.Millisecond)
 	fmt.Println("task4")
+
 }
