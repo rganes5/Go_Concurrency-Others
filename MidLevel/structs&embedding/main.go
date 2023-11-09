1f24f5b20afc45f0b87e2d612e61ab9a2ab4b805
Ganesh <ganeshraveendranit@gmail.com>
2023-11-09 20:46:18 +0530

    struct and embedding

--- MidLevel/structs&embedding/main.go
+++ MidLevel/structs&embedding/main.go
@@ -15,12 +15,14 @@ func main() {
 
 	rec2 := Rectangle{4.5, 5.6}
 	fmt.Println(rec2)
+	fmt.Println("Area of rect 2 is ", rec2.Area())
 
-	rec3 := Rectangle{
+	rec3 := &Rectangle{
 		Length: 4.5,
 		Width:  9.5,
 	}
 	fmt.Println(rec3)
+	fmt.Println("Perimeter of rect 3 is ", rec3.Perimeter())
 
 	bob := Employee{
 		ID:     1,
@@ -44,13 +46,23 @@ func main() {
 		fmt.Println("error marshalling", err.Error())
 	}
 	fmt.Println("JSON", byteSlice)
-	fmt.Printf("JSON string %s", byteSlice)
+	fmt.Printf("JSON string %s \n", byteSlice)
 
 }
 
 type Rectangle struct {
 	Length float32
 	Width  float32
+}
+
+// method with val receiver
+func (r Rectangle) Area() float32 {
+	return r.Length * r.Width
+}
+
+// method with pointer receiver
+func (r Rectangle) Perimeter() float32 {
+	return 2 * (r.Length + r.Width)
 }
 
 // Field tags
